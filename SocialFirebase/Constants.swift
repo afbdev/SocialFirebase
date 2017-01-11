c1376b1e74f5e577df7a6d9d186047b885054995
afbdev <afbdev@alexanders-mbp.attlocal.net>
2017-01-10 23:59:14 -0600

    Sign in ui custom classes complete

---
+++ SocialFirebase/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialFirebase
+//
+//  Created by afbdev on 1/10/17.
+//  Copyright © 2017 afbdev. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0 
---
+++ SocialFirebase/FancyView.swift
@@ -0,0 +1,23 @@
+//
+//  FancyView.swift
+//  SocialFirebase
+//
+//  Created by afbdev on 1/10/17.
+//  Copyright © 2017 afbdev. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+    }
+    
+}
