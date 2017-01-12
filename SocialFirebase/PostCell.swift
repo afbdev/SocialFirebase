750efd9b90680a6529c3830d71851b25e39446f2
afbdev <afbdev@alexanders-mbp.attlocal.net>
2017-01-12 01:50:32 -0600

    Completed Firebase storage download

--- SocialFirebase/PostCell.swift
+++ SocialFirebase/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
     
@@ -24,10 +25,30 @@ class PostCell: UITableViewCell {
     }
     
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, image: UIImage? = nil) {
         self.post = post
         self.caption.text = post.caption
         self.likesLabel.text = String(post.likes)
+        
+        if image != nil {
+            self.postImage.image = image
+        } else {
+            let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+            ref.data(withMaxSize: 8 * 1024 * 1024, completion: { (data, error) in
+                if (error != nil) {
+                    print("FIRStorage: Unable to download image from Firebase storage")
+                } else {
+                    print("FIRStorage: Image downloaded from Firebase storage")
+                    if let imageData = data {
+                        if let img = UIImage(data: imageData) {
+                            self.postImage.image = img
+                            FeedVC.imageCache.setObject(img, forKey: self.post.imageUrl as NSString)
+                        }
+                    }
+                }
+            })
+            
+        }
         
     }
 
