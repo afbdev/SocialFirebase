a181d1dcae723177a5ee6a484b65f4b11f42185f
afbdev <afbdev@alexanders-mbp.attlocal.net>
2017-01-11 22:59:42 -0600

    Firebase get complete

--- SocialFirebase/FeedVC.swift
+++ SocialFirebase/FeedVC.swift
@@ -14,6 +14,8 @@ class FeedVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,7 +23,17 @@ class FeedVC: UIViewController {
         tableView.dataSource = self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
         
     }
@@ -53,9 +65,12 @@ extension FeedVC: UITableViewDelegate, UITableViewDataSource {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("\(post.caption)")
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
